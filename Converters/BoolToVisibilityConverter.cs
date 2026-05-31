d8fd4941f76cbe7e07dea045fdd044cc514d363e
ZackCannaday <zackcannaday78@gmail.com>
2026-05-31 07:31:55 -0400

    Fix startup visibility binding crashes

--- Converters/BoolToVisibilityConverter.cs
+++ Converters/BoolToVisibilityConverter.cs
@@ -7,7 +7,24 @@ namespace BayBrain.Converters
     public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => (bool?)value == true ? Visibility.Visible : Visibility.Collapsed;
+        {
+            var isVisible = value switch
+            {
+                bool boolValue => boolValue,
+                int intValue => intValue > 0,
+                long longValue => longValue > 0,
+                string stringValue => !string.IsNullOrWhiteSpace(stringValue),
+                null => false,
+                _ => false
+            };
+
+            if (string.Equals(parameter as string, "invert", StringComparison.OrdinalIgnoreCase))
+            {
+                isVisible = !isVisible;
+            }
+
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => (Visibility?)value == Visibility.Visible;
--- Converters/InverseBoolToVisibilityConverter.cs
+++ Converters/InverseBoolToVisibilityConverter.cs
@@ -7,7 +7,19 @@ namespace BayBrain.Converters
     public class InverseBoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => (bool?)value == false ? Visibility.Visible : Visibility.Collapsed;
+        {
+            var isVisible = value switch
+            {
+                bool boolValue => !boolValue,
+                int intValue => intValue <= 0,
+                long longValue => longValue <= 0,
+                string stringValue => string.IsNullOrWhiteSpace(stringValue),
+                null => true,
+                _ => false
+            };
+
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => (Visibility?)value != Visibility.Visible;
